d6e8d9f99132103b1632ac6e9a5850f1ee710f8f
patrira <iradukundapatrick19@gmail.com>
2024-09-29 19:09:23 +0200

    Implementing responsiveness

--- src/app/components/trending-movies/trending-movies.component.ts
+++ src/app/components/trending-movies/trending-movies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'; 
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Movie } from '../../models/movie.model';
@@ -11,12 +11,25 @@ import { selectTrendingMovies } from '../../store/movie.selectors';
   styleUrls: ['./trending-movies.component.css'],
 })
 export class TrendingMoviesComponent implements OnInit {
-  trendingMovies$!: Observable<Movie[]>;  // Removed default initialization
+  trendingMovies$!: Observable<Movie[]>; // Observable for trending movies
+
+  @ViewChild('trendingMoviesContainer') trendingMoviesContainer!: ElementRef; // Reference to the container for scrolling
+trendingMovies: any;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     // Select trending movies from the store
     this.trendingMovies$ = this.store.select(selectTrendingMovies);
+
+    // Start automatic scrolling every 3 seconds
+    this.autoScroll();
+  }
+
+  autoScroll(): void {
+    const container = this.trendingMoviesContainer.nativeElement;
+    setInterval(() => {
+      container.scrollBy({ left: container.offsetWidth, behavior: 'smooth' });
+    }, 3000); // Adjust the interval duration as needed (3000ms = 3 seconds)
   }
 }
